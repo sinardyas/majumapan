9fa8fc61c8ac9ee45a3c68ffa955bc41bf9055db
sinardyas <sinardyas96@gmail.com>
2025-12-30 06:04:38 +0700

    temporary handle infinite loading on admin role

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-10f05c2d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.4hif9mkn9jo"
+    "revision": "0.9esfgrekeq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
